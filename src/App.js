08991019e6a5b1f138ca7316eb1a29ed2c7c7852
Steludovico <stephanie.ludovico.santos@gmail.com>
2023-05-02 14:12:30 -0300

    Listagem de Pets

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Clientes } from "./pages/Clientes/Clientes";
 import { EditaCliente } from "./pages/EditaCliente/EditaCliente";
 import { NovoPet } from "./pages/NovoPet/NovoPet";
 import { NovoServico } from "./pages/NovoServico/NovoServico";
+import { Pets } from "./pages/Pets/Pets"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/clientes" element={<Clientes />} />
           <Route path="/clientes/novo" element={<NovoCliente />} />
           <Route path="/clientes/editar/:id" element={<EditaCliente />} />
+          <Route path="/pets" element={<Pets />} />
           <Route path="/pets/novo" element={<NovoPet />} />
           <Route path="/servicos/novo" element={<NovoServico />} />
         </Route>
