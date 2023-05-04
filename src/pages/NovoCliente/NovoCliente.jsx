590c1308518eb621c4184b8488e9894162503684
Steludovico <stephanie.ludovico.santos@gmail.com>
2023-05-04 15:59:20 -0300

    IBGE

--- src/pages/NovoCliente/NovoCliente.jsx
+++ src/pages/NovoCliente/NovoCliente.jsx
@@ -1,13 +1,40 @@
 import axios from "axios";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Col, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 
 export function NovoCliente() {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
+
+    // Puxar as infirmações do IBGE
+    const [ufs, setUfs] = useState([]);
+    const [cidades, setCidades] = useState([]);
+    const [selecionarEstado, setSelecionarEstado] = useState([]);
+
+    //Estados-UF
+    useEffect(() => {
+        axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/`).then((response) => {
+            setUfs(response.data)
+        })
+    },[]);
+
+    //Cidades
+    useEffect(() => {
+        if(selecionarEstado === "0") {
+            return;
+        }
+        axios.get(
+            `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selecionarEstado}/municipios`
+        ).then((response)=>{
+            setCidades(response.data)
+        })
+    },[selecionarEstado]);
+
 
     function onSubmit(data) {
         console.log(data);
@@ -23,61 +50,158 @@ export function NovoCliente() {
     }
 
     return (
-        <div className="container">
-            <h1>Novo Cliente</h1>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Form.Group className="mb-3">
-                    <Form.Label>Nome</Form.Label>
-                    <Form.Control type="text" className={errors.nome && "is-invalid"} {...register("nome", { required: "O nome é obrigatório.", maxLength: { value: 130, message: "Limite de 130 caracteres."} })} />
-                    {errors.nome && <Form.Text className="invalid-feedback">{errors.nome.message}</Form.Text>}
-                </Form.Group>
+        <div className="justify-content-between align-items-center m-4">
+          <Row className="d-flex justify-content-center align-items-center">
 
+            <Col className="justify-content-center ms-5">
+              <h1>Novo Cliente</h1>
+              <Form className="w-75" onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group className="mb-3">
-                    <Form.Label>E-mail</Form.Label>
-                    <Form.Control type="email" className={errors.email && "is-invalid"} {...register("email", { required: "O e-mail é obrigatório.", maxLength: { value: 255, message: "Limite de 255 caracteres."} })} />
-                    {errors.email && <Form.Text className="invalid-feedback">{errors.email.message}</Form.Text>}
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control
+                    type="text"
+                    className={errors.nome && "is-invalid"}
+                    {...register("nome", {
+                      required: "O nome é obrigatório.",
+                      maxLength: {
+                        value: 130,
+                        message: "Limite de 130 caracteres.",
+                      },
+                    })}
+                  />
+                  {errors.nome && (
+                    <Form.Text className="invalid-feedback">
+                      {errors.nome.message}
+                    </Form.Text>
+                  )}
                 </Form.Group>
-
+    
                 <Form.Group className="mb-3">
-                    <Form.Label>Telefone</Form.Label>
-                    <Form.Control type="tel" className={errors.telefone && "is-invalid"} {...register("telefone", { required: "O telefone é obrigatório.", maxLength: { value: 255, message: "Limite de 255 caracteres."} })} />
-                    {errors.telefone && <Form.Text className="invalid-feedback">{errors.telefone.message}</Form.Text>}
+                  <Form.Label>E-mail</Form.Label>
+                  <Form.Control
+                    type="email"
+                    className={errors.email && "is-invalid"}
+                    {...register("email", {
+                      required: "O e-mail é obrigatório.",
+                      maxLength: {
+                        value: 255,
+                        message: "Limite de 255 caracteres.",
+                      },
+                    })}
+                  />
+                  {errors.email && (
+                    <Form.Text className="invalid-feedback">
+                      {errors.email.message}
+                    </Form.Text>
+                  )}
                 </Form.Group>
-
+    
                 <Form.Group className="mb-3">
-                    <Form.Label>Cidade</Form.Label>
-                    <Form.Control type="text" className={errors.endereco?.cidade && "is-invalid"} {...register("endereco.cidade", { required: "A cidade é obrigatória.", maxLength: { value: 255, message: "Limite de 255 caracteres."} })} />
-                    {errors.endereco?.cidade && <Form.Text className="invalid-feedback">{errors.endereco?.cidade.message}</Form.Text>}
+                  <Form.Label>Telefone</Form.Label>
+                  <Form.Control
+                    type="tel"
+                    className={errors.telefone && "is-invalid"}
+                    {...register("telefone", {
+                      required: "O telefone é obrigatório.",
+                      maxLength: {
+                        value: 255,
+                        message: "Limite de 255 caracteres.",
+                      },
+                    })}
+                  />
+                  {errors.telefone && (
+                    <Form.Text className="invalid-feedback">
+                      {errors.telefone.message}
+                    </Form.Text>
+                  )}
                 </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Form.Label>UF</Form.Label>
-                    <Form.Control type="text" className={errors.endereco?.uf && "is-invalid"} {...register("endereco.uf", { required: "O UF é obrigatório.", maxLength: { value: 2, message: "Limite de 2 caracteres."} })} />
-                    {errors.endereco?.uf && <Form.Text className="invalid-feedback">{errors.endereco?.uf.message}</Form.Text>}
-                </Form.Group>
-
-                <Form.Group className="mb-3">
+    
+                    <Form.Group className="mb-3">
+                        <Form.Label>Estado</Form.Label>
+                      <Form.Select id="uf" className={errors.endereco?.uf && "is-invalid"}{...register("endereco.uf", {required: "É obrigatório selecionar um estado"})} onChange={(event) => setSelecionarEstado(event.target.value)}>
+                        <option value="">Selecione um Estado</option>
+                        {ufs.map((uf)=>(
+                            <option key={uf.sigla} value={uf.sigla}>
+                                {uf.nome}
+                            </option>
+                        ))}
+                      </Form.Select>
+                      {errors.endereco?.uf && (
+                    <Form.Text className="invalid-feedback">
+                      {errors.endereco?.uf.message}
+                    </Form.Text>
+                  )}
+                    </Form.Group>
+    
+                    <Form.Group className="mb-3">
+                        <Form.Label>Cidade</Form.Label>
+                      <Form.Select id="cidade" className={errors.endereco?.cidade && "is-invalid"}{...register("endereco.cidade", {required: "É obrigatório selecionar uma cidade"})}>
+                        <option value="">Selecione uma Cidade</option>
+                        {cidades.map((cidade)=>(
+                            <option key={cidade.id} value={cidade.nome}>
+                                {cidade.nome}
+                            </option>
+                        ))}
+                      </Form.Select>
+                      {errors.endereco?.cidade && (
+                    <Form.Text className="invalid-feedback">
+                      {errors.endereco?.cidade.message}
+                    </Form.Text>
+                  )}
+                    </Form.Group>
+                    <Form.Group className="mb-3">
                     <Form.Label>CEP</Form.Label>
                     <Form.Control type="text" className={errors.endereco?.cep && "is-invalid"} {...register("endereco.cep", { required: "O CEP é obrigatório.", maxLength: { value: 9, message: "Limite de 9 caracteres."} })} />
                     {errors.endereco?.cep && <Form.Text className="invalid-feedback">{errors.endereco?.cep.message}</Form.Text>}
                 </Form.Group>
 
+    
                 <Form.Group className="mb-3">
-                    <Form.Label>Rua</Form.Label>
-                    <Form.Control type="text" className={errors.endereco?.rua && "is-invalid"} {...register("endereco.rua", { required: "A rua é obrigatória.", maxLength: { value: 255, message: "Limite de 255 caracteres."} })} />
-                    {errors.endereco?.rua && <Form.Text className="invalid-feedback">{errors.endereco?.rua.message}</Form.Text>}
+                  <Form.Label>Rua</Form.Label>
+                  <Form.Control
+                    type="text"
+                    className={errors.endereco?.rua && "is-invalid"}
+                    {...register("endereco.rua", {
+                      required: "A rua é obrigatória.",
+                      maxLength: {
+                        value: 255,
+                        message: "Limite de 255 caracteres.",
+                      },
+                    })}
+                  />
+                  {errors.endereco?.rua && (
+                    <Form.Text className="invalid-feedback">
+                      {errors.endereco?.rua.message}
+                    </Form.Text>
+                  )}
                 </Form.Group>
-
+    
                 <Form.Group className="mb-3">
-                    <Form.Label>Numero</Form.Label>
-                    <Form.Control type="text" className={errors.endereco?.numero && "is-invalid"} {...register("endereco.numero", { required: "O número é obrigatório.", maxLength: { value: 255, message: "Limite de 255 caracteres."} })} />
-                    {errors.endereco?.numero && <Form.Text className="invalid-feedback">{errors.endereco?.numero.message}</Form.Text>}
+                  <Form.Label>Numero</Form.Label>
+                  <Form.Control
+                    type="text"
+                    className={errors.endereco?.numero && "is-invalid"}
+                    {...register("endereco.numero", {
+                      required: "O número é obrigatório.",
+                      maxLength: {
+                        value: 255,
+                        message: "Limite de 255 caracteres.",
+                      },
+                    })}
+                  />
+                  {errors.endereco?.numero && (
+                    <Form.Text className="invalid-feedback">
+                      {errors.endereco?.numero.message}
+                    </Form.Text>
+                  )}
                 </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Cadastrar
+    
+                <Button variant="dark" type="submit">
+                  Cadastrar
                 </Button>
-            </Form>
+              </Form>
+            </Col>
+          </Row>
         </div>
-    );
-}
+      );
+    }
